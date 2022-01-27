30c4c9b40dce4123f73aaec8653c90d3298406ff
Andreas Hofer <andreas.hofer@amitroncis.de>
2022-01-27 10:24:07 +0000

    Update run.sh

--- {{cookiecutter.project_short_name}}/run.sh
+++ {{cookiecutter.project_short_name}}/run.sh
@@ -3,4 +3,5 @@ echo "The output file is stored in the folder output."
 pandoc --template=ressources/templates/eisvogelEigeneTitelseite.tex \
 --listings --pdf-engine=xelatex --pdf-engine-opt=--shell-escape --metadata-file=metadata.yml \
 --citeproc -o output/out.pdf src/?_??-*.md
+echo "Successfully Build the pdf!"
 read  -n 1 -p "Press any key to close this window."
