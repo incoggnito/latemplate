461b9a1c89871cd952a9e91a11dec9812cecb84f
Andreas Hofer <andreas.hofer@amitronics.de>
2022-01-27 10:07:43 +0100

    fix bugs

--- {{cookiecutter.project_short_name}}/run.sh
+++ {{cookiecutter.project_short_name}}/run.sh
@@ -2,5 +2,5 @@ echo "Don't worry, if it takes some time (up to 5 min) for compiling."
 echo "The output file is stored in the folder output."
 pandoc --template=ressources/templates/eisvogelEigeneTitelseite.tex \
 --listings --pdf-engine=xelatex --pdf-engine-opt=--shell-escape --metadata-file=metadata.yml \
---citeproc -o output/CosmoDuAbschlussbericht.pdf src/?_??-*.md
+--citeproc -o output/out.pdf src/?_??-*.md
 read  -n 1 -p "Press any key to close this window."
