58763a88ff86bad2fec7e4f055ad89358dd72109
Andreas Hofer <andreas.hofer@amitroncis.de>
2022-10-15 15:53:43 +0000

    Update run.sh

--- {{cookiecutter.project_short_name}}/run.sh
+++ {{cookiecutter.project_short_name}}/run.sh
@@ -3,11 +3,11 @@ echo "The output file is stored in the folder output."
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 pandoc --template=ressources/templates/eisvogelEigeneTitelseite.tex \
 --listings --pdf-engine=xelatex --pdf-engine-opt=--shell-escape --metadata-file=metadata.yml \
---filter=pandoc-citeproc --filter=pandoc-svg.py --toc --top-level-division=chapter -o output/out.pdf main.md
+--filter=pandoc-citeproc --toc --top-level-division=chapter -o output/out.pdf main.md
 else
 pandoc --template=ressources/templates/eisvogelEigeneTitelseite.tex \
 --listings --pdf-engine=xelatex --pdf-engine-opt=--shell-escape --metadata-file=metadata.yml \
---citeproc --filter=pandoc-svg.py --toc --top-level-division=chapter -o output/out.pdf main.md
+--citeproc --toc --top-level-division=chapter -o output/out.pdf main.md
 fi
 echo "Successfully Build the pdf!"
 read  -n 1 -p "Press any key to close this window."
