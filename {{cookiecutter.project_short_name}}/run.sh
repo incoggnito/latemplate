21622d2ea9b9d0ed621e31f824a2cae2d1e20965
Andreas Hofer <andreas.hofer@amitroncis.de>
2022-10-15 15:42:47 +0000

    Update run.sh

--- {{cookiecutter.project_short_name}}/run.sh
+++ {{cookiecutter.project_short_name}}/run.sh
@@ -1,7 +1,13 @@
 echo "Don't worry, if it takes some time (up to 5 min) for compiling."
 echo "The output file is stored in the folder output."
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 pandoc --template=ressources/templates/eisvogelEigeneTitelseite.tex \
 --listings --pdf-engine=xelatex --pdf-engine-opt=--shell-escape --metadata-file=metadata.yml \
---citeproc -o output/out.pdf src/?_??-*.md
+--filter=pandoc-citeproc --filter=pandoc-svg.py --toc --top-level-division=chapter -o output/out.pdf main.md
+else
+pandoc --template=ressources/templates/eisvogelEigeneTitelseite.tex \
+--listings --pdf-engine=xelatex --pdf-engine-opt=--shell-escape --metadata-file=metadata.yml \
+--citeproc --filter=pandoc-svg.py --toc --top-level-division=chapter -o output/out.pdf main.md
+fi
 echo "Successfully Build the pdf!"
 read  -n 1 -p "Press any key to close this window."
