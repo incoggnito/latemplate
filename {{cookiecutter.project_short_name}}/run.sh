2e9d43a42f90ecd8f26b12b1905e8ad40cf5e58a
Andreas Hofer <andreas.hofer@amitroncis.de>
2022-12-08 17:03:53 +0000

    Update run.sh

--- {{cookiecutter.project_short_name}}/run.sh
+++ {{cookiecutter.project_short_name}}/run.sh
@@ -1,13 +1,15 @@
 echo "Don't worry, if it takes some time (up to 5 min) for compiling."
 echo "The output file is stored in the folder output."
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+echo "Linux detected!"
 pandoc --template=ressources/templates/eisvogelEigeneTitelseite.tex \
 --listings --pdf-engine=xelatex --pdf-engine-opt=--shell-escape --metadata-file=metadata.yml \
 --filter=pandoc-citeproc --toc --top-level-division=chapter -o output/out.pdf main.md
 else
+echo "Windows detected!"
 pandoc --template=ressources/templates/eisvogelEigeneTitelseite.tex \
 --listings --pdf-engine=xelatex --pdf-engine-opt=--shell-escape --metadata-file=metadata.yml \
---citeproc --toc --top-level-division=chapter -o output/out.pdf main.md
+--filter=pandoc-citeproc --toc --top-level-division=chapter -o output/out.pdf main.md
 fi
 echo "Successfully Build the pdf!"
 read  -n 1 -p "Press any key to close this window."
